d75546a3ee300eab952106a4d0364fc77e5f3fac
BeataMaro <beatamaro@interia.pl>
2024-02-07 23:21:09 +0100

    remove base defineConfig

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/budget/',
+  // base: '/budget-app/',
   plugins: [react(), svgr()],
 });
